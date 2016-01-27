5cc738b96ecd6e0fa95805c6abbcbfcabb72faa1
Jay-Plumb <jaymjplumb@gmail.com>
2016-01-27 15:59:29 +0000

    Initial Commit

---
+++ movies/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  movies
+//
+//  Created by Jason Plumb on 2016. 1. 27..
+//  Copyright © 2016년 JasonPlumb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
